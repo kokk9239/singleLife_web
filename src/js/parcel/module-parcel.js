b074d954b8f7a82a97efbcaaf33773f4c6ea98b5
choi <chaose37@naver.com>
2016-08-11 12:03:52 +0900

    Update module-parcel.js

--- src/js/parcel/module-parcel.js
+++ src/js/parcel/module-parcel.js
@@ -6,9 +6,10 @@ var parcel_module = (function () {
 
     function showParcel(code,invoice) {
         $.ajax({
-            url : "http://14.32.66.116:11001/controller/parcel?apikey=a5589d81bc7d511f3becc51e093ef239",
+            url : "http://14.32.66.116:11001/controller/parcel",
             dataType : "JSON",
             data : {
+                apikey:"a5589d81bc7d511f3becc51e093ef239",
                 code : code,
                 invoice:invoice
             },
